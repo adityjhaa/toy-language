f0454536636e190d94f5ca6da89bb2e4975f4082
adityjha0 <aditya2005aps@gmail.com>
2024-03-19 09:17:28 +0530

    more operators

--- basic/toy.ml
+++ basic/toy.ml
@@ -135,7 +135,7 @@ let rec stkmc s c = match s, c with
 
 (*Adding Variables*)
 
-type exp = Num of int | Bl of myBool | V of string | Plus of exp * exp | Times of exp * exp | And of exp * exp | Or of exp * exp | Not of exp | Eq of exp * exp | Gt of exp * exp | IfTE  of exp * exp * exp  | Pair of exp * exp | Fst of exp | Snd of exp | Lets of string * exp * exp;;
+type exp = Num of int | Bl of myBool | V of string | Plus of exp * exp | Times of exp * exp | Minus of exp * exp | Div of exp * exp | And of exp * exp | Or of exp * exp | Not of exp | Eq of exp * exp | Gt of exp * exp | Lt of exp * exp | IfTE  of exp * exp * exp  | Pair of exp * exp | Fst of exp | Snd of exp | Lets of string * exp * exp;;
 
 let rec ht e = 
   match e with
@@ -144,11 +144,14 @@ let rec ht e =
   | V x -> 0
   | Plus (e1, e2) -> 1 + (max (ht e1) (ht e2))
   | Times (e1, e2) -> 1 + (max (ht e1) (ht e2))
+  | Minus (e1, e2) -> 1 + (max (ht e1) (ht e2))
+  | Div (e1, e2) -> 1 + (max (ht e1) (ht e2))
   | And (e1, e2) -> 1 + (max (ht e1) (ht e2))
   | Or (e1, e2) -> 1 + (max (ht e1) (ht e2))
   | Not e1 -> 1 + (ht e1)
   | Eq (e1, e2) -> 1 + (max (ht e1) (ht e2))
   | Gt (e1, e2) -> 1 + (max (ht e1) (ht e2))
+  | Lt (e1, e2) -> 1 + (max (ht e1) (ht e2))
   | IfTE (e0, e1, e2) -> 1 + (max (ht e0) (max (ht e1) (ht e2)))
   | Pair (e1, e2) -> 1+ (max (ht e1) (ht e2))
   | Fst (e0) -> 1 + (ht e0)
@@ -163,11 +166,14 @@ let rec size e =
   | V x -> 1
   | Plus (e1, e2) -> 1 + (size e1) + (size e2)
   | Times (e1, e2) -> 1 + (size e1) + (size e2)
+  | Minus (e1, e2) -> 1 + (size e1) + (size e2)
+  | Div (e1, e2) -> 1 + (size e1) + (size e2)
   | And (e1, e2) -> 1 + (size e1) + (size e2)
   | Or (e1, e2) -> 1 + (size e1) + (size e2)
   | Not e1 -> 1 + (size e1)
   | Eq (e1, e2) -> 1 + (size e1) + (size e2)
   | Gt (e1, e2) -> 1 + (size e1) + (size e2)
+  | Lt (e1, e2) -> 1 + (size e1) + (size e2)
   | IfTE (e0, e1, e2) -> 1 + (size e0) + (size e1) + (size e2)
   | Pair (e1, e2) -> 1 + (size e1) + (size e2)
   | Fst (e0) -> 1 + (size e0)
@@ -202,6 +208,12 @@ let rec eval (e: exp) (rho: (string * values) list) : values =
   | Times (e1, e2) -> let N n1 = (eval e1 rho)
                       and N n2 = (eval e2 rho)
                     in N (n1 * n2)
+  | Minus (e1, e2) -> let N n1 = (eval e1 rho)
+                      and N n2 = (eval e2 rho)
+                    in N (n1 - n2)
+  | Div (e1, e2) -> let N n1 = (eval e1 rho)
+                      and N n2 = (eval e2 rho)
+                    in (if n2 <> 0 then N (n1 / n2) else raise Division_by_zero)
   | And (e1, e2) -> let B b1 = (eval e1 rho)
                       and B b2 = (eval e2 rho)
                     in B (b1 && b2)
@@ -216,6 +228,9 @@ let rec eval (e: exp) (rho: (string * values) list) : values =
   | Gt (e1, e2) -> let N n1 = (eval e1 rho)
                       and N n2 = (eval e2 rho)
                     in B (n1 > n2)
+  | Lt (e1, e2) -> let N n1 = (eval e1 rho)
+                      and N n2 = (eval e2 rho)
+                    in B (n1 < n2)
   | IfTE (e0, e1, e2) -> let B b0 = (eval e0 rho)
                     in if b0 then (eval e1 rho)
                       else (eval e2 rho)
@@ -232,7 +247,7 @@ let rec eval (e: exp) (rho: (string * values) list) : values =
 ;;
 
 
-type opcode = LDN of int | LDB of bool | LOOKUP of string | PLUS | TIMES | AND | OR | NOT | EQ | GT | COND of opcode list * opcode list | PAIR | FST | SND | LET of string * opcode list;;
+type opcode = LDN of int | LDB of bool | LOOKUP of string | PLUS | TIMES | MINUS | DIV | AND | OR | NOT | EQ | GT | LT | COND of opcode list * opcode list | PAIR | FST | SND | LET of string * opcode list;;
 
 let rec compile e = 
   match e with
@@ -241,11 +256,14 @@ let rec compile e =
   | V x -> [LOOKUP x]
   | Plus (e1, e2) -> (compile e1) @ (compile e2) @ [PLUS]
   | Times (e1, e2) -> (compile e1) @ (compile e2) @ [TIMES]
+  | Minus (e1, e2) -> (compile e1) @ (compile e2) @ [MINUS]
+  | Div (e1, e2) -> (compile e1) @ (compile e2) @ [DIV]
   | And (e1, e2) -> (compile e1) @ (compile e2) @ [AND]
   | Or (e1, e2) -> (compile e1) @ (compile e2) @ [OR]
   | Not e1 -> (compile e1) @ [NOT]
   | Eq (e1, e2) -> (compile e1) @ (compile e2) @ [EQ]
   | Gt (e1, e2) -> (compile e1) @ (compile e2) @ [GT]
+  | Lt (e1, e2) -> (compile e1) @ (compile e2) @ [LT]
   | IfTE (e0, e1, e2) -> (compile e0) @ [COND(compile e1, compile e2)]
   | Pair (e1, e2) -> (compile e1) @ (compile e2) @ [PAIR]
   | Fst e0 -> (compile e0) @ [FST]
@@ -263,11 +281,14 @@ let rec stkmc g s c =
   | s, (LOOKUP x)::c' -> stkmc g ((find g x)::s) c'
   | (N n2)::(N n1)::s', PLUS::c' -> stkmc g (N(n1+n2)::s') c'
   | (N n2)::(N n1)::s', TIMES::c' -> stkmc g (N(n1*n2)::s') c'
+  | (N n2)::(N n1)::s', MINUS::c' -> stkmc g (N(n1-n2)::s') c'
+  | (N n2)::(N n1)::s', DIV::c' -> if n2 <> 0 then (stkmc g (N(n1/n2)::s') c') else raise Division_by_zero
   | (B b1)::(B b2)::s', AND::c' -> stkmc g (B(b1 && b2)::s') c'
   | (B b1)::(B b2)::s', OR::c' -> stkmc g (B(b1 || b2)::s') c'
   | (B b1)::s', NOT::c' -> stkmc g (B(not b1)::s') c'
   | (N n2)::(N n1)::s', EQ::c' -> stkmc g (B(n1=n2)::s') c'
   | (N n2)::(N n1)::s', GT::c' -> stkmc g (B(n1>n2)::s') c'
+  | (N n2)::(N n1)::s', LT::c' -> stkmc g (B(n1<n2)::s') c'
   | (B true)::s', COND(c1, c2)::c' -> stkmc g s' (c1 @ c')
   | (B false)::s', COND(c1, c2)::c' -> stkmc g s' (c2 @ c')
   | v2::v1::s', PAIR::c' -> stkmc g (P(v1, v2)::s') c'
